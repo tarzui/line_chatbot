6d3c1728fbc1aa56ebf504ceb74a269421e46906
kiitar <chakri.psu@gmail.com>
2020-07-09 14:31:21 +0700

    up

--- config.js
+++ config.js
@@ -2,9 +2,12 @@ const Config = {
   port: 50000,
   API_URL: "https://api.line.me/v2/bot/message",
   channelID: "",
-  channelSecret: "cfda8ff87c425bbd40830a9da08af1e2",
+  // channelSecret: "cfda8ff87c425bbd40830a9da08af1e2",
+  // channelAccessToken:
+  //   "bHNX+qQ+FHczPrJTQS0NStlPj+saScVTMvCynELcKPOTM22DJo55pJO7Igmz2A+etjI8ZnA0z1excTDNgio+UhZ4r8d7wEU9fjmbXe4vGLfRv40UgUtNkbKEmhEL1KUf7rPpKidi//BSPwxlcSeGowdB04t89/1O/w1cDnyilFU=",
+  channelSecret: "8095d104a215c199a54c25af140ebf34",
   channelAccessToken:
-    "bHNX+qQ+FHczPrJTQS0NStlPj+saScVTMvCynELcKPOTM22DJo55pJO7Igmz2A+etjI8ZnA0z1excTDNgio+UhZ4r8d7wEU9fjmbXe4vGLfRv40UgUtNkbKEmhEL1KUf7rPpKidi//BSPwxlcSeGowdB04t89/1O/w1cDnyilFU=",
+    "gnO3JGouYl2SoBJ6Akogn2tAcJDvbQ4kEnVrTkwYC5xs7BiF4L2ICqaZBTJB/X3zyzuh700ovVUyA9n6+ZbExAqzVDTb3xvRWEVeaHFs0m3IQcCsZMguWhPR8WBQtz8xAS45YWwIwahDZVQbeNEssAdB04t89/1O/w1cDnyilFU=",
 };
 
 module.exports = Config;
--- v1.0/index.js
+++ v1.0/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 // const { replyMessage, pushMessage, getProfile } = require("./api_line_message");
 const Config = require("../config");
 const Client = require("@line/bot-sdk").Client;
-
+const axios = require("axios");
 const client = new Client({
   channelAccessToken: Config.channelAccessToken,
   channelSecret: Config.channelSecret,
@@ -10,12 +10,44 @@ const client = new Client({
 
 const router = express.Router();
 
+router.get("/test", (req, res) => {
+  res.send("Hello line bot");
+});
+
 router.post("/", (req, res) => {
   const event = req.body.events[0];
-  client.replyMessage(event.replyToken, {
-    type: "text",
-    text: `I'm TaR Bot Test you Said : ${event.message.text}`,
+
+  let data = JSON.stringify({
+    query:
+      "query message($message:String!){\n    message(message:$message){\n    data{\n      found\n      intent\n      confidence\n    }\n  }\n}",
+    variables: { message: `${event.message.text}` },
   });
+
+  var config = {
+    method: "post",
+    url: "http://103.245.164.59:3004/api_management",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: data,
+  };
+
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data.data.message.data.intent));
+      let rep = response.data.data.message.data.intent;
+      client.replyMessage(event.replyToken, {
+        type: "text",
+        text: `${rep}`,
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  // client.replyMessage(event.replyToken, {
+  //   type: "text",
+  //   text: `I'm TaR Bot Test you Said : ${event.message.text}`,
+  // });
   return;
 });
 
